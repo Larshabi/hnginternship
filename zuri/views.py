17c781b3a4b65227298033851b1261201e7b84eb
Lasabi Olalekan <84998804+Larshabi@users.noreply.github.com>
2022-10-30 12:41:41 +0100

    correction

--- zuri/views.py
+++ zuri/views.py
@@ -7,6 +7,6 @@ from rest_framework import status
 class Home(GenericAPIView):
     def get(self, request):
         data = {
-        "slackUsername":"Lash", "Backend":True, "Age":23, "Bio":"My name is Lasabi Olalekan Muqeet."}
+        "slackUsername":"Lash", "backend":True, "age":22, "bio":"My name is Lasabi Olalekan Muqeet."}
         return Response(data, status=status.HTTP_200_OK)
     
